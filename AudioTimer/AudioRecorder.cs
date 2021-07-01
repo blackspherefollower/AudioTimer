a401058daa3fbaad40fd4f810b08dfd024c91a96
blackspherefollower <darktech@black-sphere.co.uk>
2021-07-01 05:20:17 -0700

    Add project files.

---
+++ AudioTimer/AudioRecorder.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+
+namespace AudioTimer
+{
+    class AudioRecorder
+    {
+        private WaveInEvent _waveSource;
+
+        private int _samples;
+        private float _max;
+        private float _omax;
+        private float _omin = float.MaxValue;
+        private List<float> _peaks = new List<float>();
+        private long _startTime;
+        private long _stopTime;
+        private TaskCompletionSource<bool> _tcs;
+        private WaveFileWriter _writer;
+
+        public void Start(string recording = null)
+        {
+            _peaks = new List<float>();
+            _samples = 0;
+            _max = 0;
+            _omax = 0;
+            _omin = float.MaxValue;
+
+            _waveSource = new WaveInEvent
+            {
+                WaveFormat = new WaveFormat(48000, 16, 1)
+            };
+
+            //Console.WriteLine(waveSource.WaveFormat.AverageBytesPerSecond + "bps");
+
+            var nsp = new NotifyingSampleProvider(new WaveToSampleProvider(new Wave16ToFloatProvider(new WaveInProvider(_waveSource))));
+
+            nsp.Sample += NspOnSample;
+
+            if (recording != null)
+            {
+                _writer = new WaveFileWriter(recording, _waveSource.WaveFormat);
+            }
+
+            _waveSource.RecordingStopped += waveSource_RecordingStopped;
+            _waveSource.DataAvailable += delegate(object sender, WaveInEventArgs args)
+            {
+                _writer?.Write(args.Buffer, 0, args.BytesRecorded);
+                var sampleBuffer = new float[args.BytesRecorded/2];
+                nsp.Read(sampleBuffer, 0, sampleBuffer.Length);
+            };
+
+            _startTime = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+            _waveSource.StartRecording();
+        }
+
+        private void NspOnSample(object sender, SampleEventArgs e)
+        {
+            _max = Math.Max(_max, e.Left);
+            _samples++;
+
+            if (_samples % 480 == 0)
+            {
+                _peaks.Add(_max);
+                _omax = Math.Max(_omax, _max);
+                _omin = Math.Min(_omin, _max);
+                _samples = 0;
+                _max = 0;
+            }
+        }
+
+        public void Stop()
+        {
+            _tcs = new TaskCompletionSource<bool>();
+            _waveSource.StopRecording();
+            _tcs.Task.Wait();
+        }
+
+        void waveSource_RecordingStopped(object sender, StoppedEventArgs e)
+        {
+            _stopTime = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+            if (_waveSource != null)
+            {
+                _waveSource.Dispose();
+                _waveSource = null;
+            }
+
+            if (_writer != null)
+            {
+                _writer.Dispose();
+                _writer = null;
+            }
+
+            _tcs?.TrySetResult(true);
+        }
+
+        public void Report()
+        {
+            if (_peaks == null || !_peaks.Any())
+            {
+                Console.WriteLine("No data recorded!");
+                return;
+            }
+
+            // Sample rate 
+
+            float r = _omax - _omin;
+            _peaks.ForEach(p =>
+            {
+                int n = Convert.ToInt16(((p-_omin) / r) * 20);
+                Console.WriteLine($"{new string('|', n)}{new string(' ', 20-n)}{n}");
+            });
+
+
+            Console.WriteLine($"Range {_omin}-{_omax}");
+            Console.WriteLine($"Recorded {_peaks.Count / 100.0}s of data");
+            Console.WriteLine($"Realtime {(_stopTime-_startTime) / 1000.0}s of data");
+        }
+
+        public long NoisePeriod(float threshold)
+        {
+            if (_peaks == null || !_peaks.Any())
+            {
+                //Console.WriteLine("No data recorded!");
+                return 0;
+            }
+
+            float r = _omax - _omin;
+            int start = -1;
+            int end = -1;
+            int pos = 0;
+            while(start < 0 && pos < _peaks.Count)
+            {
+                var n = (_peaks[pos] - _omin) / r;
+                if (n > threshold)
+                    start = pos;
+                pos++;
+            }
+
+            pos = _peaks.Count - 1;
+            while (end < 0 && pos >= 0)
+            {
+                var n = (_peaks[pos] - _omin) / r;
+                if (n > threshold)
+                    end = pos;
+                pos--;
+            }
+
+            if (start < 0 || end < 0)
+            {
+                return 0;
+            }
+
+            return (end - start) * 10;
+        }
+    }
+}
---
+++ AudioTimer/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Buttplug;
+
+namespace AudioTimer
+{
+    class Program
+    {
+        private static async Task WaitForKey()
+        {
+            Console.WriteLine("Press any key to continue.");
+            while (!Console.KeyAvailable)
+            {
+                await Task.Delay(1);
+            }
+            Console.ReadKey(true);
+        }
+
+        private static Task move(ButtplugClientDevice dev, uint speed, uint position)
+        {
+            return dev.SendRawWriteCmd(Endpoint.Tx, new byte[] { 0x03, (byte)speed, (byte)position }, false);
+        }
+
+        static async Task Main(string[] args)
+        {
+            TaskCompletionSource<ButtplugClientDevice> _tcs = new TaskCompletionSource<ButtplugClientDevice>();
+            ButtplugClient bp = new ButtplugClient("timer_client");
+            
+
+            bp.DeviceAdded += (sender, eventArgs) =>
+            {
+                Console.WriteLine($"Found device: {eventArgs.Device.Name}");
+                if (eventArgs.Device.Name.Contains("Vorze Piston"))
+                {
+                    _tcs.SetResult(eventArgs.Device);
+                }
+            };
+            bp.ErrorReceived += (sender, eventArgs) => {
+                Console.WriteLine($"Bang: {eventArgs.Exception.Message}");
+                _tcs.SetException(eventArgs.Exception);
+
+            };
+            bp.ServerDisconnect += (sender, eventArgs) =>
+            {
+                Console.WriteLine("Server disconnected!");
+                _tcs.SetCanceled();
+            };
+
+            await bp.ConnectAsync(new ButtplugEmbeddedConnectorOptions()
+            {
+                AllowRawMessages = true,
+                ServerName = "timer_server"
+            });
+
+            await bp.StartScanningAsync();
+            Console.WriteLine($"Looking for Vorze SA Piston");
+
+            var dev = await _tcs.Task;
+            if (dev == null)
+            {
+                return;
+            }
+
+            Console.WriteLine($"Found the test device");
+            await WaitForKey();
+
+            var audio = new AudioRecorder();
+
+            await move( dev, 20, 0 );
+            Thread.Sleep(1000);
+
+            var log = new System.IO.StreamWriter(@"piston.csv", true);
+
+            var pos = 0;
+            var lastPos = 0;
+            var time = 0L;
+
+            Console.WriteLine($"Testing forward {pos}");
+            audio.Start();
+            await move( dev, 20, 200 );
+            Thread.Sleep(2000);
+            audio.Stop();
+            time = audio.NoisePeriod(0.25f);
+            audio.Report();
+            await WaitForKey();
+
+            await move(dev, 20, 0);
+            Thread.Sleep(2000);
+
+            // loop speeds first
+            for (int speed = 20; speed <= 60; speed+=5 )
+            {
+                Console.WriteLine($"Testing speed {speed}");
+
+                for (int range = 200; range > 0; range -= 10)
+                {
+                    Console.WriteLine($"Testing range {range}");
+
+                    var file = "rec-" + DateTime.Now.ToFileTime() + ".wav";
+                    lastPos = pos;
+                    pos += range;
+                    Console.WriteLine($"Testing forward {pos}");
+                    audio.Start(file);
+                    await move(dev, (uint)speed, (uint)pos);
+                    Thread.Sleep(2000);
+                    audio.Stop();
+                    time = audio.NoisePeriod(0.25f);
+                    Console.WriteLine($"Found {time}ms of noise");
+                    log.WriteLine($"forward,{speed},{range},{time},{lastPos},{pos},{file}");
+                    Thread.Sleep(1000);
+
+                    file = "rec-" + DateTime.Now.ToFileTime() + ".wav";
+                    lastPos = pos;
+                    pos -= range;
+                    Console.WriteLine($"Testing reverse {pos}");
+                    audio.Start(file);
+                    await move(dev, (uint)speed, (uint)pos);
+                    Thread.Sleep(2000);
+                    audio.Stop();
+                    time = audio.NoisePeriod(0.25f);
+                    Console.WriteLine($"Found {time}ms of noise");
+                    log.WriteLine($"reverse,{speed},{range},{time},{lastPos},{pos},{file}");
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
